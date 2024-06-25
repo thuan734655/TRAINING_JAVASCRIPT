c34dfb04e2779ed5a10eadeb1a8560265a3e5ea3
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-06-25 20:17:21 +0700

    Finish chapter 1, 1/2 chapter 2

---
+++ chapter2.js
@@ -0,0 +1,8 @@
+// This is a single-line comment.
+
+/* This is also a comment */ // and here is another comment.
+
+/*
+ * This is a multi-line comment. The extra * characters at the start of
+ * each line are not a required part of the syntax; they just look cool!
+ */
